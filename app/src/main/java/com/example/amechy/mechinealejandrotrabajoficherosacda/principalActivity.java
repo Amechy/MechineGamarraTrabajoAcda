8e82733a538aae60665887afe85ef8d4ebe29eed
Amechy <alejandromechinegamarra@gmail.com>
2017-12-10 21:42:23 +0100

    amos al lio

---
+++ app/src/main/java/com/example/amechy/mechinealejandrotrabajoficherosacda/principalActivity.java
@@ -0,0 +1,15 @@
+package com.example.amechy.mechinealejandrotrabajoficherosacda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class principalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+
+    }
+}
